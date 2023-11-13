eb936d87bcc66c7ea11a1f25b6a6c9e3e1f17598
SHADIMK7 <muhammedshadi.mk7@gmail.com>
2023-11-13 10:23:38 +0530

    FINAL

--- NativeApp/forms.py
+++ NativeApp/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from cities_light.models import Country, City
 
 class CountryCityForm(forms.Form):
-    country = forms.ModelChoiceField(queryset=Country.objects.filter(name='India'), empty_label="Select a country")
+    country = forms.ModelChoiceField(queryset=Country.objects.filter(name='India'), empty_label="Select a country", widget=forms.Select(attrs={'class': 'custom-select'}))
     
-    city = forms.ModelChoiceField(queryset=City.objects.filter(country__name='India'), empty_label="Select a city")
+    city = forms.ModelChoiceField(queryset=City.objects.filter(country__name='India'), empty_label="Select a city", widget=forms.Select(attrs={'class': 'custom-select'}))
 
 class CityFilterForm(forms.Form):
-    city = forms.ModelChoiceField(queryset=City.objects.filter(country__name='India'), empty_label="All Cities")    
+    city = forms.ModelChoiceField(queryset=City.objects.filter(country__name='India'), empty_label="All Cities", widget=forms.Select(attrs={'class': 'custom-select'}))    
